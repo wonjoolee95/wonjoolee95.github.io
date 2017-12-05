62b1af9632210da04f26788bc638fff70dfed3a8
Won Joo Lee <leewj2@uci.edu>
2017-12-05 12:05:52 -0800

    Add findings.html and update visuals for index.html

--- js/script.js
+++ js/script.js
@@ -1,3 +1,31 @@
+// Wrap every letter in a span
+$('.ml1 .letters').each(function(){
+  $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+});
+
+anime.timeline({loop: false})
+	.add({
+		targets: '.ml1 .letter',
+		scale: [0.3,1],
+		opacity: [0,1],
+		translateZ: 0,
+		easing: "easeOutExpo",
+		duration: 600,
+		delay: function(el, i) {
+		return 70 * (i+1)
+	}
+	}).add({
+		targets: '.ml1 .line',
+		scaleX: [0,1],
+		opacity: [0.5,1],
+		easing: "easeOutExpo",
+		duration: 700,
+		offset: '-=875',
+		delay: function(el, i, l) {
+		return 80 * (l - i);
+	}
+});
+
 $(document).ready(function() {
 
 	// Needed for SmoothScroll.js
@@ -11,3 +39,6 @@ $(document).ready(function() {
 	});
 
 });
+
+window.sr = ScrollReveal();
+sr.reveal(".section-content");
